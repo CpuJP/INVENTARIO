f5fd2c328dca2b232a7f3f4cff929589ace96bb4
CpuJP <cpujuanpis@gmail.com>
2022-11-12 22:40:24 -0500

    solución de CORS para conexion con angular

--- src/main/java/microservicios/demo/controller/MicroController.java
+++ src/main/java/microservicios/demo/controller/MicroController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/micro")
 public class MicroController {

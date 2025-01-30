67be74c969fb5cb6057a14f4aa1fd3f7042924b4
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2025-01-30 15:25:57 -0500

    first commit

---
+++ src/main/java/com/inventariokyqa/inventariokyqa/InventarioKyqaApplication.java
@@ -0,0 +1,13 @@
+package com.inventariokyqa.inventariokyqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioKyqaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioKyqaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inventariokyqa/inventariokyqa/InventarioKyqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventariokyqa.inventariokyqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioKyqaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
